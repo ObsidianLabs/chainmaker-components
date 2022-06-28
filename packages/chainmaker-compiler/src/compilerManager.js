99c6e5d05525b0184f02eea4c4f360236fa9be14
flowfire <ziyi.meng@obsidians.io>
2022-06-28 08:52:39 +0800

    compiler

---
+++ packages/chainmaker-compiler/src/compilerManager.js
@@ -0,0 +1,356 @@
+import { DockerImageChannel } from '@obsidians/docker'
+import notification from '@obsidians/notification'
+import fileOps from '@obsidians/file-ops'
+import stripAnsi from 'strip-ansi'
+import { modelSessionManager } from '@obsidians/code-editor'
+import { t } from '@obsidians/i18n'
+
+import SolcjsCompiler from './SolcjsCompiler'
+import soljsonReleases from './soljsonReleases.json'
+
+class SolcjsChannel extends DockerImageChannel {
+  installed() {
+    return true
+  }
+
+  versions() {
+    const versions = Object.entries(soljsonReleases).map(([Tag, Name]) => ({ Tag, Name }))
+    const event = new CustomEvent('versions', { detail: versions })
+    this.eventTarget.dispatchEvent(event)
+    return versions
+  }
+}
+
+export class CompilerManager {
+  static button = null
+  static terminal = null
+  static truffleTerminal = null
+
+  constructor() {
+    this.truffle = new DockerImageChannel(process.env.DOCKER_IMAGE_COMPILER)
+    this.solc = new SolcjsChannel()
+    this.notification = null
+    this.solcjsCompiler = new SolcjsCompiler()
+  }
+
+  get projectRoot() {
+    if (!CompilerManager.terminal) {
+      throw new Error('CompilerTerminal is not instantiated.')
+    }
+    return CompilerManager.terminal.props.cwd
+  }
+
+  focus() {
+    if (CompilerManager.terminal) {
+      CompilerManager.terminal.focus()
+    }
+  }
+
+  async execute(cmd) {
+    CompilerManager.switchCompilerConsole('terminal')
+    return await CompilerManager.terminal?.exec(cmd)
+  }
+
+  async cacheSolcBin(url, version) {
+    const cacheStorage = await window.caches.open('solcjs')
+    try {
+      if (await cacheStorage.match(url)) {
+        return
+      }
+    } catch {
+      await cacheStorage.delete(url)
+    }
+
+    this.notification = notification.info(t('contract.build.downloadingSolcBin'), `Downloading <b>${version}</b>...`, 0)
+    const request = new Request(url, { mode: 'no-cors' })
+    const response = await fetch(request)
+    await cacheStorage.put(url, response)
+    this.notification.dismiss()
+  }
+
+  async buildBySolcjs(projectManager) {
+    if (!await projectManager.isMainValid) {
+      notification.error(t('contract.build.noMainFile'), t('contract.build.noMainFileText'))
+      throw new Error('No Main File.')
+    }
+
+    const solcVersion = projectManager.projectSettings.get('compilers.solc')
+    const solcFileName = soljsonReleases[solcVersion]
+
+    // TODO: use the production proxy temporally
+    const solcUrl = `https://ide.black/solc/${solcFileName}`
+
+    const evmVersion = projectManager.projectSettings.get('compilers.evmVersion')
+    const optimizer = projectManager.projectSettings.get('compilers.optimizer')
+
+    CompilerManager.button.setState({ building: true })
+    try {
+      await this.cacheSolcBin(solcUrl, solcFileName)
+    } catch (e) {
+      console.error(e)
+      CompilerManager.button.setState({ building: false })
+      throw e
+    }
+
+    CompilerManager.terminal.writeCmdToTerminal(`solcjs --bin ${projectManager.projectSettings.get('main')}`, `[${solcFileName}]`)
+    this.notification = notification.info(t('contract.build.start'), `${t('contract.build.building')}...`, 0)
+
+    let output
+    try {
+      output = await this.solcjsCompiler.compile(solcUrl, projectManager)
+    } catch (e) {
+      this.notification.dismiss()
+      notification.error(t('contract.build.fail'), e.message)
+      CompilerManager.button.setState({ building: false })
+      throw e
+    }
+
+    if (!output) {
+      this.notification.dismiss()
+      notification.error(t('contract.build.fail'), ``)
+      CompilerManager.button.setState({ building: false })
+      throw new Error('Build Failed.')
+    }
+
+    if (output.contracts) {
+      for (const file in output.contracts) {
+        for (const contractName in output.contracts[file]) {
+          const json = output.contracts[file][contractName]
+          const contractJsonPath = projectManager.pathForProjectFile(`build/contracts/${contractName}.json`)
+          const contractJson = JSON.stringify(json, null, 2)
+          await projectManager.saveFile(contractJsonPath, contractJson)
+        }
+      }
+      projectManager.refreshDirectory()
+      projectManager.refreshDirectory(projectManager.pathForProjectFile('build/contracts'))
+    }
+    const errorDecorations = []
+    let hasError = false
+
+    output.errors?.forEach(error => {
+      let color
+      if (error.severity === 'error') {
+        hasError = true
+        color = '--color-danger'
+        errorDecorations.push(this.parseSolcJSBuild(error))
+      } else if (error.severity === 'warning') {
+        color = '--color-warning'
+      }
+      CompilerManager.terminal.writeToTerminal(error.type, color)
+      CompilerManager.terminal.writeToTerminal(`${error.formattedMessage.replace(error.type, '').replace(/\n/g, '\n\r')}`)
+    })
+
+    this.notification.dismiss()
+    CompilerManager.button.setState({ building: false })
+    if (hasError) {
+      notification.error(t('contract.build.fail'), t('contract.build.fileCodeErr'))
+      modelSessionManager.updateDecorations(errorDecorations)
+    } else {
+      notification.success(t('contract.build.success'), t('contract.build.successTextSmart'))
+      modelSessionManager.clearDecoration('compiler')
+    }
+  }
+
+  async build(settings, projectManager, sourceFile) {
+    if (projectManager.remote) {
+      return await this.buildBySolcjs(projectManager)
+    }
+
+    const { framework, compilers = {} } = settings
+    const projectRoot = this.projectRoot
+    CompilerManager.button.setState({ building: 'checking' })
+
+    // if (framework.endsWith('-docker')) {
+    //   if (!compilers || !compilers[process.env.COMPILER_VERSION_KEY]) {
+    //     notification.error(t('contract.build.noVersion', { compilerName: process.env.COMPILER_NAME }), t('contract.build.noVersionText', { compilerName: process.env.COMPILER_NAME }))
+    //     CompilerManager.button.setState({ building: false })
+    //     throw new Error(`No ${process.env.COMPILER_NAME} version.`)
+    //   }
+    //   const allVersions = await this.truffle.versions()
+    //   if (!allVersions.find(v => v.Tag === compilers[process.env.COMPILER_VERSION_KEY])) {
+    //     notification.error(t('contract.build.notInstalled', { compilerName: process.env.COMPILER_NAME, compilerVersion: compilers[process.env.COMPILER_VERSION_KEY]}), t('contract.build.notInstalledText', { compilerName: process.env.COMPILER_NAME }))
+    //     CompilerManager.button.setState({ building: false })
+    //     throw new Error(`${process.env.COMPILER_NAME} version not installed`)
+    //   }
+    // }
+
+    // // if (!compilers.solc) {
+    // //   notification.error('No Solc Version', `Please select a version for solc in project settings.`)
+    // //   throw new Error('No solc version.')
+    // // }
+
+    // const allSolcVersions = await this.solc.versions()
+    // if (compilers.solc && compilers.solc !== 'default' && !allSolcVersions.find(v => v.Tag === compilers.solc)) {
+    //   notification.error(t('contract.build.solcNotInstall', { compilerSolc: compilers.solc }), t('contract.build.solcNotInstallText'))
+    //   CompilerManager.button.setState({ building: false })
+    //   throw new Error('Solc version not installed')
+    // }
+
+    CompilerManager.button.setState({ building: true })
+    CompilerManager.switchCompilerConsole('terminal')
+    // if (!sourceFile) {
+    //   this.notification = notification.info(t('contract.build.start'), t('contract.build.building'), 0)
+    // } else {
+    //   this.notification = notification.info(t('contract.build.contractFile'), `Building <b>${sourceFile}</b>...`, 0)
+    // }
+
+    const cmd = this.generateBuildCmd({ projectRoot, settings, sourceFile })
+    let result
+    if (typeof cmd === 'string') {
+      result = await CompilerManager.terminal.exec(cmd)
+    } else if (cmd instanceof Array) {
+      for (let eachCmd of cmd) {
+        result = await CompilerManager.terminal.exec(cmd) 
+      }
+    } else {
+      throw new Error('Wrong command type')
+    }
+
+
+    CompilerManager.button.setState({ building: false })
+    this.notification.dismiss()
+    await CompilerManager.terminal.execAsChildProcess(`docker rm $(docker ps --filter status=exited --filter ancestor=ethereum/solc:${compilers.solc} -q)`)
+
+    if (result.code === 130) {
+      if (result.code === 130) {
+        notification.error('Build Terminated')
+        return { errors: [] }
+      }
+    }
+
+    const { errors, decorations } = this.parseBuildLogs(stripAnsi(result.logs))
+    if (result.code) {
+      if (errors.length) {
+        notification.error(t('contract.build.fail'), errors[0])
+      } else {
+        notification.error(t('contract.build.fail'), t('contract.build.fileCodeErr'))
+      }
+      return { errors, decorations }
+    }
+
+    const buildFolder = projectManager.path.join(framework === 'hardhat' ? 'artifacts' : 'build', 'contracts')
+    if (!sourceFile) {
+      notification.success(t('contract.build.projectSuccess'), t('contract.build.fileSuccessText', { buildFolder }))
+    } else {
+      notification.success(t('contract.build.fileSuccess'), t('contract.build.fileSuccessText', { buildFolder }))
+    }
+    return { decorations }
+  }
+
+  static async stop() {
+    if (CompilerManager.terminal) {
+      CompilerManager.terminal.execAsChildProcess(`docker stop -t 1 truffle-compile`)
+      await CompilerManager.terminal.stop()
+      // await CompilerManager.terminal.execAsChildProcess(`docker rm $(docker ps --filter status=exited --filter ancestor=ethereum/solc:${compilers.solc} -q)`)
+    }
+  }
+
+  parseSolcJSBuild(error) {
+    const { prefix: projectPrefix, userId, projectId } = modelSessionManager.projectManager
+    const [prefix] = error.formattedMessage.match(/(?<=:).+(?=:)/g)
+    const filePath =  error.sourceLocation.file
+    const [row, column] = prefix.split(':')
+    const lines = error.formattedMessage.split('\n')
+    const length = lines[lines.length - 1].trim().length
+
+    return {
+      filePath: `${projectPrefix}/${userId}/${projectId}/${filePath.replace('./', '')}`,
+      text: `[Solcjs Compiler]: ${error.message}`,
+      row: Number(row),
+      length,
+      type: 'error',
+      column: Number(column),
+      from: 'compiler'
+    }
+  }
+
+  parseBuildLogs(msg) {
+    let index
+    index = msg.indexOf('Compiling your contracts...')
+    if (index > -1) {
+      msg = msg.substr(index + 30)
+    }
+    const lines = msg.split('\n')
+
+    const errors = []
+    let decorations = []
+    let status = ''
+    let currentBlock = ''
+    lines.map(line => line.trim()).forEach(line => {
+      if (!line) {
+        if (status === 'ERROR') {
+          errors.push(currentBlock.trim())
+        } else if (status === 'DECORATION') {
+          decorations.push(currentBlock.trim())
+        }
+        status = ''
+        currentBlock = ''
+      } else if (line.startsWith('Error: ') || status === 'ERROR') {
+        status = 'ERROR'
+        currentBlock += (line + '\n')
+      } else if (line.startsWith(',/')) {
+        if (status === 'DECORATION') {
+          decorations.push(currentBlock.trim())
+        }
+        status === 'DECORATION'
+        currentBlock = (line.substr(1) + '\n')
+      } else if (line.startsWith('/') || status === 'DECORATION') {
+        status = 'DECORATION'
+        currentBlock += (line + '\n')
+      }
+    })
+    decorations = decorations.map(msg => {
+      const lines = msg.split('\n')
+      const [prefix, ...rest] = lines[0].split(': ')
+      const [filePath, row, column] = prefix.split(':')
+      const text = rest.join(': ').trim()
+      let type = 'error'
+      if (text.startsWith('Warning: ')) {
+        type = 'warning'
+      }
+      if (row && column) {
+        const length = lines[lines.length - 1].trim().length
+        return { filePath, type, row: Number(row), column: Number(column), length, text }
+      } else {
+        return { filePath, type, text }
+      }
+    })
+    return { errors, decorations }
+  }
+
+  generateBuildCmd({ projectRoot, settings, sourceFile }) {
+    const { framework, npmClient, compilers } = settings
+    const projectDir = fileOps.current.getDockerMountPath(projectRoot)
+
+    switch (framework) {
+      default: 
+
+      break;
+    }
+
+    if (!framework.endsWith('-docker')) {
+      const npmRun = npmClient === 'yarn' ? npmClient : `${npmClient} run`
+      return `${npmRun} build`
+    }
+
+    // const cmd = [
+    //   `docker run -d --name chainmaker-solidity-contract -v $WORK_DIR:/home chainmakerofficial/chainmaker-solidity-contract:2.0.0 bash -c "while true; do echo hello world; sleep 5;done"
+    //   `
+    // ]
+
+    const cmd = [
+      `docker run -t --rm --name solidity-compile`,
+      `-v "${projectDir}:/home"`,
+      `--platform linux/amd64`,
+      `${process.env.DOCKER_IMAGE_COMPILER}:2.0.0`,
+      `bash -c "cd /data`,
+      `&& tar xvf contract_solidity_template.tar.gz`,
+      `&& cd /home`,
+      `&& solc --abi --bin --hashes --overwrite -o . token.sol"`
+    ]
+
+    return cmd.join(' ')
+  }
+}
+
+export default new CompilerManager()
